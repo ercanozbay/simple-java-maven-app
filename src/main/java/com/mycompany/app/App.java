13a16810d328d8893ca79ee214729e85e77dbe20
Ercan Ozbay <ercanozbay@gmail.com>
2020-04-03 13:15:31 +0300

    son commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,7 +7,7 @@ public class App
 {
 
     private final String message = "Hello World!";
-    private final String message2 = "Hello Worldasd!";
+    private final String message2 = "Hello deneme!";
 
     public App() {}
 
